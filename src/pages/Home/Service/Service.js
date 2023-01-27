7e60c993d75698e4047f1527ebd693b2e1ed775a
Aymanoli <aymanoli777@gmail.com>
2023-01-27 23:59:35 +0600

    new

--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -5,11 +5,12 @@ const Service = ({service}) => {
     // const {service} = props;
     const {name, price, description, img} = service;
     return (
-        <div className='service'>
+        <div className='service pb-3'>
             <img className='img' src={img} alt="" />
             <h3>{name}</h3>
             <h5>Price: {price}</h5>
             <p className='px-5'>{description}</p>
+            <button className='btn btn-warning'>Book {name.toLowerCase()}</button>
         </div>
     );
 };
